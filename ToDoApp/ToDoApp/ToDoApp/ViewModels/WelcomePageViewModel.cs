70a5f7319b55e20c79ea0c921c0e6dcf9147ed77
dangbalinh <linha1xp@gmail.com>
2022-12-26 13:29:33 +0700

    first commit

---
+++ ToDoApp/ToDoApp/ToDoApp/ViewModels/WelcomePageViewModel.cs
@@ -0,0 +1,35 @@
+﻿using Prism.Navigation;
+using System.Windows.Input;
+using ToDoApp.Views;
+using Xamarin.Forms;
+
+namespace ToDoApp.ViewModels
+{
+    public class WelcomePageViewModel: BaseViewModel
+    {
+        
+
+        public ICommand GetStartedCommand { get; set; }
+
+      
+
+    
+
+        public WelcomePageViewModel(
+            INavigationService navigationService) : base(navigationService)
+        { 
+            GetStartedCommand = new Command(GetStartedCommandHandler);
+        }
+
+       
+
+      
+
+        private async void GetStartedCommandHandler()
+        {
+            await _navigationService.NavigateAsync(nameof(AuthPage));
+        }
+
+        
+    }
+}
